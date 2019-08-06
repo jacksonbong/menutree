4b6bbb8a2d883b2a7bd9b3d5a293d96a16fb9412
Chai Wui Ze <31319344+jacksonbong@users.noreply.github.com>
2019-08-06 11:34:54 +0800

    Update menutree.js

--- menutree.js
+++ menutree.js
@@ -22,14 +22,14 @@
 		   function loopmenu(length, menu,alignleftmenu){
 			  for (var z = 0; z < length;z++){
 					var menu_ = menu.children[1].children[z];
-					console.log('menu2',menu_);
+					
 					if (menu_.children.length > 1 ) {
 						if (alignleftmenu) { 
 							menu_.children[1].className ="menu2 margin-left";
 						} else {
 							menu_.children[1].className ="menu2";
 						}
-						console.log('menu2',menu_.children[1]);
+						
 						var menu__len = menu_.children[1].children.length;
 						if (menu_.children[1].children) {
 							loopmenu(menu__len,menu_, alignleftmenu);
@@ -46,13 +46,13 @@
 				  for (var y in data) {
 					  htmlul +="<li><a href='javascript:void(0)'>" + (jQuery.type(settings.data[y]) == 'object' ? y : data[y])+"</a>";
 						if (jQuery.type(data[y]) == 'object' || $.isArray(data[y])) {
-							console.log('ulloop',loopdata(data[y]));
+							
 							htmlul += loopdata(data[y]);
 						}
 				  }
 				  
 				  htmlul += "</li></ul>"
-				  console.log('ul',htmlul);
+				
 				return htmlul;
 		   }
 		   /** init function**/ 
@@ -62,7 +62,7 @@
 				  that.find('ul').attr("hidden","hidden");
 				  
 				  if (settings.layer== true) {
-					  console.log(that,"that obj");
+					 
 					  that.bind('layercss',function(){  
 					
 						console.log($(this));
@@ -91,7 +91,7 @@
 								}
 								
 								menu2len = menu1.children[1].children.length;
-								console.log(menu2len);
+								
 								loopmenu(menu2len,menu1,alignleftmenu);
 								
 							} 
@@ -100,7 +100,7 @@
 					
 					that.trigger('layercss');
 				  } 
-				  console.log('obj',that);
+				
 				 that.find('li').on('mouseenter',function(event){
 					var menu1 = $(this)[0].children[1];
 					
@@ -147,7 +147,7 @@
 			init(settings.target);
 		
 		  } else {
-			  console.log('hi');
+			  
 			  init($(this));
 		  }
 		}
